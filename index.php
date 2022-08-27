83f1a93dc36f12bb5f5a4bae486bdedaaba217c5
ZribeDev <66210834+ZribeDev@users.noreply.github.com>
2022-08-27 13:09:52 +0000

    added script llist

--- index.php
+++ index.php
@@ -2,19 +2,21 @@
 <html>
 	<head>
 		<meta charset="utf-8">
-		<title>Home Page</title>
+		<title>Skripts</title>
 		<link href="style.css" rel="stylesheet" type="text/css">
-		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+		<script src="https://kit.fontawesome.com/2b4733a4e7.js" crossorigin="anonymous"></script>
 	</head>
 	<body class="loggedin">
 		<nav class="navtop">
 			<div>
-				<h1>Website Title</h1>
+				<h1>Skripts</h1>
+        <a href="index.php"><i class="fa-solid fa-house"></i>Home</a>
+				<a href="skripts.php"><i class="fa-solid fa-code"></i>Skripts</a>
 			</div>
 		</nav>
 		<div class="content">
 			<h2>Home Page</h2>
-			<p>Welcome back, <?=$_SESSION['name']?>!</p>
+			<p>Welcome back!</p>
 		</div>
 	</body>
 </html>
---
+++ skripts.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Skripts</title>
+		<link href="style.css" rel="stylesheet" type="text/css">
+		<script src="https://kit.fontawesome.com/2b4733a4e7.js" crossorigin="anonymous"></script>
+	</head>
+	<body class="loggedin">
+		<nav class="navtop">
+			<div>
+				<h1>Skripts</h1>
+        <a href="index.php"><i class="fa-solid fa-house"></i>Home</a>
+				<a href="skripts.php"><i class="fa-solid fa-code"></i>Skripts</a>
+			</div>
+		</nav>
+		<div class="content">
+			<h2>Skripts</h2>
+      <pre class="code">
+      <?php 
+        $path = "scripts";
+        
+        if ($handle = opendir($path)) {
+          while (false !== ($file = readdir($handle))) {
+              if ('.' === $file) continue;
+              if ('..' === $file) continue;
+              $text = file_get_contents("scripts/".$file);
+              echo "
+              <p>
+              <div class=\"codebox\">
+              <code>   
+var message = \"".$text."\";
+alert(message);
+              </code>
+              </div>
+              </p>
+              ";
+          }
+          closedir($handle);
+        }
+      ?>
+		</div>
+	</body>
+</html>
